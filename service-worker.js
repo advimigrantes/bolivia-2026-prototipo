3b6e8d1e33092a511e6aba4617bbe9880152aaea
Andressa Alexandre <contato@advocaciaparaimigrantes.com.br>
2026-09-23 05:56:54 -0300

    Simplificar ícone de roteiro do app

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* O shell é público; os dados reais ficam somente no snapshot local do navegador. */
-const CACHE_NAME = "bolivia-2026-v5";
-const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./css/styles.css","./js/data.js","./js/maps.js","./js/api.js","./js/app.js","./assets/logo-pldf-com-fundo.png","./assets/foto-perfil.jpg","./assets/aviao.png","./assets/icon-bolivia.svg","./assets/icon-bolivia-180.png","./assets/icon-bolivia-192.png","./assets/icon-bolivia-512.png"];
+const CACHE_NAME = "bolivia-2026-v6";
+const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./css/styles.css","./js/data.js","./js/maps.js","./js/api.js","./js/app.js","./assets/logo-pldf-com-fundo.png","./assets/foto-perfil.jpg","./assets/aviao.png","./assets/icon-roteiro-32.png","./assets/icon-roteiro-180.png","./assets/icon-roteiro-192.png","./assets/icon-roteiro-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
